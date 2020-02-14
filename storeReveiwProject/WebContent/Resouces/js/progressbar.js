aeb3fbc7798f235bf655a34a7de65fd27fe9098e
minjinKK <33615248+minjinKK@users.noreply.github.com>
2020-02-14 16:03:54 +0900

    resouce

---
+++ storeReveiwProject/WebContent/Resouces/js/progressbar.js
@@ -0,0 +1,45 @@
+/**
+   * ProgressBar
+   */
+var progress = $('.progress2');
+
+progress.each(function () {
+
+	var _self = $(this);
+	var progressNumber = _self.find('.progress2__number');
+	progressNumber.text('0%');
+
+	_self.waypoint(function (direction) {
+		var progressBar = _self.find('.progress2__bar'),
+			    delay = progressBar.data("delay"),
+			    durations = progressBar.data("duration"),
+			    timing = progressBar.data("timing"),
+			    getPercent = progressBar.data('progress-percent');
+		
+		console.log(durations);
+
+		progressBar.css({
+			'width': getPercent + '%',
+			'transition': 'all ' + durations + 'ms ' + timing,
+			'transition-delay': delay + 'ms'
+		});
+
+		setTimeout(function () {
+			progressNumber.prop('Counter', 0).animate({
+				Counter: getPercent
+			}, {
+				duration: durations,
+				easing: 'swing',
+				step: function step(now) {
+					$(this).text(Math.ceil(now) + '%');
+				}
+			});
+		}, delay);
+
+		this.destroy();
+	}, {
+		offset: function offset() {
+			return Waypoint.viewportHeight() - _self.outerHeight() - 150;
+		}
+	});
+});
